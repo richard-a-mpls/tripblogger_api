4d1c377da99376de8fb6ef045b69e287e06588fa
mousedrool <richard.abrahamson@yahoo.com>
2021-06-28 08:14:46 -0500

    initial swagger and codegen buildout

---
+++ source/main/utilities/generate_swagger.sh
@@ -0,0 +1,1 @@
+java -jar /usr/local/Cellar/swagger-codegen/3.0.26/libexec/swagger-codegen-cli.jar generate -i /Users/richardabrahamson/PycharmProjects/tripblogger_api/source/main/swagger/swagger.yaml -o /Users/richardabrahamson/PycharmProjects/tripblogger_api/source/main/generated/ -l python-flask
