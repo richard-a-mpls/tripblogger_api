29a60d781a5704f17cc82f65aa1f28a4b7f11021
mousedrool <richard.abrahamson@yahoo.com>
2021-06-29 08:43:43 -0500

    added base profile management and associate profile to session.

--- source/main/generated/swagger_server/models/profile.py
+++ source/main/generated/swagger_server/models/profile.py
@@ -14,29 +14,39 @@ class Profile(Model):
 
     Do not edit the class manually.
     """
-    def __init__(self, profile_id: str=None, profile_name: str=None, connections: List=None):  # noqa: E501
+    def __init__(self, profile_id: str=None, profile_name: str=None, identity_issuer: str=None, identity_id: str=None, connections: List=None):  # noqa: E501
         """Profile - a model defined in Swagger
 
         :param profile_id: The profile_id of this Profile.  # noqa: E501
         :type profile_id: str
         :param profile_name: The profile_name of this Profile.  # noqa: E501
         :type profile_name: str
+        :param identity_issuer: The identity_issuer of this Profile.  # noqa: E501
+        :type identity_issuer: str
+        :param identity_id: The identity_id of this Profile.  # noqa: E501
+        :type identity_id: str
         :param connections: The connections of this Profile.  # noqa: E501
         :type connections: List
         """
         self.swagger_types = {
             'profile_id': str,
             'profile_name': str,
+            'identity_issuer': str,
+            'identity_id': str,
             'connections': List
         }
 
         self.attribute_map = {
             'profile_id': 'profile_id',
             'profile_name': 'profile_name',
+            'identity_issuer': 'identity_issuer',
+            'identity_id': 'identity_id',
             'connections': 'connections'
         }
         self._profile_id = profile_id
         self._profile_name = profile_name
+        self._identity_issuer = identity_issuer
+        self._identity_id = identity_id
         self._connections = connections
 
     @classmethod
@@ -93,6 +103,48 @@ class Profile(Model):
             raise ValueError("Invalid value for `profile_name`, must not be `None`")  # noqa: E501
 
         self._profile_name = profile_name
+
+    @property
+    def identity_issuer(self) -> str:
+        """Gets the identity_issuer of this Profile.
+
+
+        :return: The identity_issuer of this Profile.
+        :rtype: str
+        """
+        return self._identity_issuer
+
+    @identity_issuer.setter
+    def identity_issuer(self, identity_issuer: str):
+        """Sets the identity_issuer of this Profile.
+
+
+        :param identity_issuer: The identity_issuer of this Profile.
+        :type identity_issuer: str
+        """
+
+        self._identity_issuer = identity_issuer
+
+    @property
+    def identity_id(self) -> str:
+        """Gets the identity_id of this Profile.
+
+
+        :return: The identity_id of this Profile.
+        :rtype: str
+        """
+        return self._identity_id
+
+    @identity_id.setter
+    def identity_id(self, identity_id: str):
+        """Sets the identity_id of this Profile.
+
+
+        :param identity_id: The identity_id of this Profile.
+        :type identity_id: str
+        """
+
+        self._identity_id = identity_id
 
     @property
     def connections(self) -> List:
