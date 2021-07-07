c083edd7be2b1a974d6f292669f67b66ba5e4d44
mousedrool <richard.abrahamson@yahoo.com>
2021-07-07 14:41:32 -0500

    interim commit

--- source/main/generated/swagger_server/__main__.py
+++ source/main/generated/swagger_server/__main__.py
@@ -2,12 +2,14 @@
 
 import connexion
 from swagger_server import encoder
+from flask_cors import CORS
 
 
 def main():
     app = connexion.App(__name__, specification_dir='./swagger/')
     app.app.json_encoder = encoder.JSONEncoder
     app.add_api('swagger.yaml', arguments={'title': 'API Title'}, pythonic_params=True)
+    CORS(app.app)
     app.run(port=8080)
 
 
