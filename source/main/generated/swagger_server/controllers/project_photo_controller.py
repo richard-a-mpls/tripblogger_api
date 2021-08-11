4649c91c3d3b8458177470e7b9f487a991ae64dc
mousedrool <50158729+mousedrool@users.noreply.github.com>
2021-08-11 15:58:23 -0500

    Add project photos (#11)
    * initial commit for add photos to project endpoint.
    
    * new file transformation class, takes a file input from browser and transforms and if necessary reduces it's size.
    
    * new entry to not overwrite.
    
    * new db accessor method to add project photos.
    
    * refactor to remove duplicated code.
    
    * refactor definitions to allow array of photos per project and new upload endpoint for those photos.
    
    * refactor to remove the photo upload as part of the create project.
    
    * refactor to remove duplicated code and leverage the new file transformation utility

---
+++ source/main/generated/swagger_server/controllers/project_photo_controller.py
@@ -0,0 +1,42 @@
+import connexion
+from swagger_server.extensions.mongo_interface import MongoInterface
+from swagger_server.utilities.file_transformation import FileTransformation
+
+
+def add_project_photo(project_id, file_name=None):  # noqa: E501
+    """upload a photo to your project.
+
+    upload a photo to your project # noqa: E501
+
+    :param project_id: ID of project to delete
+    :type project_id: str
+    :param file_name: 
+    :type file_name: strstr
+
+    :rtype: None
+    """
+
+    user_profile = connexion.request.authorization["user_profile"]
+    m_interface = MongoInterface()
+    project = m_interface.get_project(project_id)
+
+    if project["profile_id"] != user_profile:
+        print ("profile ID does not match")
+        return {"status": 401, "reason": "Not Authorized"}
+
+    uploaded_file = connexion.request.files['file']
+
+
+    file_transform = FileTransformation()
+    photo = file_transform.transform(uploaded_file)
+
+    header = uploaded_file.stream.read(512)
+    uploaded_file.stream.seek(0)
+
+    m_interface = MongoInterface()
+    inserted_id = m_interface.create_photo(photo.to_dict())
+    #TODO, define proper return object and refactor.
+    #return {'id': inserted_id}
+    m_interface.append_project_photo(project_id, inserted_id)
+
+    return {'id': inserted_id}
