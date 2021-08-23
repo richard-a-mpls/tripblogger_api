e823275a250ba0ed5f08c92a99d06efa77cfb962
mousedrool <richard.abrahamson@yahoo.com>
2021-08-23 14:16:29 -0500

    tweak to allow for b2c login + b2c federated login.

--- source/main/python/authorization_extensions.py
+++ source/main/python/authorization_extensions.py
@@ -9,22 +9,26 @@ from swagger_server.utilities import b2c_token_validator
 class AuthorizationExtensions:
 
     def process_authentication(self, graph_domain, token):
-        graph_domain = "b2c"
+        idp = "b2c"
         print (token)
         decoded_token = b2c_token_validator.extract_token(str(token))
         print(decoded_token)
 
+        if 'idp' in decoded_token:
+            idp = decoded_token['idp']
+
         m_interface = MongoInterface()
-        found_profile = m_interface.get_profile_by_identity(graph_domain, decoded_token["sub"])
+        found_profile = m_interface.get_profile_by_identity(idp, decoded_token["sub"])
         if found_profile is None:
             # need to create a new profile
             profile_json = {
                 "identity_id": decoded_token["sub"],
-                "identity_issuer": graph_domain,
+                "identity_issuer": idp,
                 "profile_name": decoded_token["name"]
             }
+
             m_interface.create_profile(profile_json)
-            found_profile = m_interface.get_profile_by_identity(graph_domain, decoded_token["sub"])
+            found_profile = m_interface.get_profile_by_identity(idp, decoded_token["sub"])
 
         jwt_contents = {"profile_id": str(found_profile["_id"]), "profile_name": found_profile["profile_name"], "expires": int(time.time())*100000}
         encoded_jwt = jwt.encode(jwt_contents, os.environ["jwt_secret"], algorithm="HS256")
