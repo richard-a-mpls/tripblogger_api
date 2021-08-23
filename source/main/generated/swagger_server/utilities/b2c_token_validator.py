832a1590951864e285ee61ce1fb0db74b692bf5a
mousedrool <50158729+mousedrool@users.noreply.github.com>
2021-08-23 12:13:58 -0500

    Azure b2c (#13)
    * look up the correct profile to return.
    
    * adjust to use b2c instead of facebook

---
+++ source/main/generated/swagger_server/utilities/b2c_token_validator.py
@@ -0,0 +1,81 @@
+import jwt
+from swagger_server.utilities.jwksutils import rsa_pem_from_jwk  # <-- this module contains the piece of code described previously
+import time
+# obtain jwks as you wish: configuration file, HTTP GET request to the endpoint returning them;
+
+jwks_url = "https://rcaazdemo.b2clogin.com/rcaazdemo.onmicrosoft.com/b2c_1_bloggersignin/discovery/v2.0/keys"
+token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImtpZCI6Ilg1ZVhrNHh5b2pORnVtMWtsMll0djhkbE5QNC1jNTdkTzZRR1RWQndhTmsifQ.eyJleHAiOjE2Mjk1NzQwODMsIm5iZiI6MTYyOTU3MDQ4MywidmVyIjoiMS4wIiwiaXNzIjoiaHR0cHM6Ly9yY2FhemRlbW8uYjJjbG9naW4uY29tLzYyYzgyZDliLTcyZmQtNDgwNS1iZmZmLTlhODQ4ZGUxNjc5Ni92Mi4wLyIsInN1YiI6ImE2OGU4NDU4LWNlZDEtNGI3NC05MjBjLWJkZmY2ZWEzYzcwMCIsImF1ZCI6IjkzNzVkYmEyLTRlNWYtNDM3Ny04NjNlLWIyMzFjNjA4ZWFkYSIsIm5vbmNlIjoiY2RmYjY4ZTEtMzJkMS00N2FiLTkxNGYtYTg5OGYzNzRlMDA0IiwiaWF0IjoxNjI5NTcwNDgzLCJhdXRoX3RpbWUiOjE2Mjk1NzA0ODMsIm9pZCI6ImE2OGU4NDU4LWNlZDEtNGI3NC05MjBjLWJkZmY2ZWEzYzcwMCIsIm5hbWUiOiJSaWNoIFVzZXIiLCJlbWFpbHMiOlsicmNhQHJpY2guY28iXSwidGZwIjoiQjJDXzFfQkxvZ2dlclNpZ25pbiJ9.UugiHqjOAQh3_1GsbQC8I3eYAGTXxUyiA_OL_c1XTwzMPXek8IAyHKDRL8O8vSeVhSDsYRfWmrUDfgfbFXKP2wlHPM_vHFSp7MYC8h6H2RaRKTNhK112-oz0z9i20vvKDqpIDXicBSginox3UIVE_OaKsbDxi8RqJkR89r2l8r249SOYJvofEINW6zSSxmJVts-S4gzWRatxCS-8V_SXS6iM-9WeAsaeShiox3ZM5Wz7Ku2txe-w24-ZXlXoaOMJSWJTlKxcf43iZmjNwYd_MSc4lyvK3FvaTFiRb-zJ6ocUdAV5FCr0rdTYNNuXlafUVBVStayTWRpbhB9i--8low"
+jwks = {
+    "keys": [
+        {"kid":"X5eXk4xyojNFum1kl2Ytv8dlNP4-c57dO6QGTVBwaNk","nbf":1493763266,"use":"sig","kty":"RSA","e":"AQAB","n":"tVKUtcx_n9rt5afY_2WFNvU6PlFMggCatsZ3l4RjKxH0jgdLq6CScb0P3ZGXYbPzXvmmLiWZizpb-h0qup5jznOvOr-Dhw9908584BSgC83YacjWNqEK3urxhyE2jWjwRm2N95WGgb5mzE5XmZIvkvyXnn7X8dvgFPF5QwIngGsDG8LyHuJWlaDhr_EPLMW4wHvH0zZCuRMARIJmmqiMy3VD4ftq4nS5s8vJL0pVSrkuNojtokp84AtkADCDU_BUhrc2sIgfnvZ03koCQRoZmWiHu86SuJZYkDFstVTVSR0hiXudFlfQ2rOhPlpObmku68lXw-7V-P7jwrQRFfQVXw"}
+    ]
+}
+
+# configuration, these can be seen in valid JWTs from Azure B2C:
+# TODO set these as env vars
+valid_audiences = ['3a5d7d6a-6380-48dc-b027-7fb3a6270409']  # id of the application prepared previously
+issuer = 'https://rcaazdemo.b2clogin.com/62c82d9b-72fd-4805-bfff-9a848de16796/v2.0/'  # iss
+
+
+class InvalidAuthorizationToken(Exception):
+    def __init__(self, details):
+        super().__init__('Invalid authorization token: ' + details)
+
+
+def get_kid(token):
+    headers = jwt.get_unverified_header(token)
+    if not headers:
+        raise InvalidAuthorizationToken('missing headers')
+    try:
+        return headers['kid']
+    except KeyError:
+        raise InvalidAuthorizationToken('missing kid')
+
+
+def get_jwk(kid):
+    for jwk in jwks.get('keys'):
+        if jwk.get('kid') == kid:
+            return jwk
+    raise InvalidAuthorizationToken('kid not recognized')
+
+
+def get_public_key(token):
+    return rsa_pem_from_jwk(get_jwk(get_kid(token)))
+
+
+def validate_jwt(jwt_to_validate):
+    public_key = get_public_key(jwt_to_validate)
+    time.sleep(.2) # we seem to be 1 ms ahead of the iat and nbf timestamps
+    decoded = jwt.decode(jwt_to_validate,
+                         public_key,
+                         verify=True,
+                         algorithms=['RS256'],
+                         audience=valid_audiences,
+                         issuer=issuer)
+
+    # do what you wish with decoded token:
+    # if we get here, the JWT is validated
+    print(decoded)
+    return decoded
+
+
+def extract_token(token):
+    import traceback
+
+    print ("validate " + token);
+    if not token:
+        print('Please pass a valid JWT')
+
+    try:
+        decoded = validate_jwt(token)
+    except Exception as ex:
+        traceback.print_exc()
+        print('The JWT is not valid!')
+        return None;
+    else:
+        print('The JWT is valid!')
+    return decoded
+
+
+# if __name__ == '__main__':
+#     main()
--- source/main/python/authorization_extensions.py
+++ source/main/python/authorization_extensions.py
@@ -4,66 +4,28 @@ import jwt
 import os
 import time
 from swagger_server.extensions.mongo_interface import MongoInterface
+from swagger_server.utilities import b2c_token_validator
 
 class AuthorizationExtensions:
 
-    check_token_url = "https://graph.facebook.com/debug_token?input_token={}&access_token={}"
-    user_details_url = "https://graph.facebook.com/{}?fields=id,name,email&access_token={}"
-
     def process_authentication(self, graph_domain, token):
-        check_token_response = self.check_token(token)
-        if not check_token_response:
-            print ("check token response returned False")
-            return False
+        graph_domain = "b2c"
+        print (token)
+        decoded_token = b2c_token_validator.extract_token(str(token))
+        print(decoded_token)
 
         m_interface = MongoInterface()
-        user_details_json = self.get_user_details(check_token_response['user_id'], token)
-        # TODO need to auto create profile if one isn't already in, and associate to session
-        found_profile = m_interface.get_profile_by_identity("fb", user_details_json["id"])
+        found_profile = m_interface.get_profile_by_identity(graph_domain, decoded_token["sub"])
         if found_profile is None:
             # need to create a new profile
             profile_json = {
-                "identity_id": user_details_json["id"],
+                "identity_id": decoded_token["sub"],
                 "identity_issuer": graph_domain,
-                "profile_name": user_details_json["name"]
+                "profile_name": decoded_token["name"]
             }
             m_interface.create_profile(profile_json)
-            found_profile = m_interface.get_profile_by_identity(graph_domain, user_details_json["id"])
-
-
+            found_profile = m_interface.get_profile_by_identity(graph_domain, decoded_token["sub"])
 
         jwt_contents = {"profile_id": str(found_profile["_id"]), "profile_name": found_profile["profile_name"], "expires": int(time.time())*100000}
         encoded_jwt = jwt.encode(jwt_contents, os.environ["jwt_secret"], algorithm="HS256")
         return encoded_jwt
-
-    def check_token(self, token):
-        response = requests.get(self.get_fb_token_url(token));
-        json_resp = response.json()
-        print ("resp is " + str(json_resp))
-        print ("App ID: " + json_resp['data']['app_id'])
-
-        if json_resp['data']["is_valid"] != True:
-            print ("token is_valid responded with not True")
-            return False
-
-        if os.environ["fb_app_id"] != json_resp['data']['app_id']:
-            print ("incorrect app id issued for fb")
-            return False
-
-        if int(time.time()) > json_resp['data']['expires_at']:
-            print ("token has expired from fb")
-            return False
-
-        return json_resp['data']
-
-    def get_user_details(self, user_id, token):
-        response = requests.get(self.get_fb_user_url(user_id, token))
-        json_resp = response.json()
-        print ("name, email " + json_resp["name"] + ":" + json_resp["email"])
-        return json_resp
-
-    def get_fb_token_url(self, token):
-        return self.check_token_url.format(token, token)
-
-    def get_fb_user_url(self, user_id, token):
-        return self.user_details_url.format(user_id, token)
