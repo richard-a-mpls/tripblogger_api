8dc6126cb4f6f8aa592662d2218e333898c18def
mousedrool <richard.abrahamson@yahoo.com>
2021-08-24 10:10:13 -0500

    further reduce the size of files stored.

--- source/main/generated/swagger_server/utilities/file_transformation.py
+++ source/main/generated/swagger_server/utilities/file_transformation.py
@@ -18,17 +18,20 @@ class FileTransformation:
         photo.type = imghdr.what(None, header)
         photo.data = Binary(uploaded_file.stream.read());
 
-        if os.fstat(uploaded_file.fileno()).st_size > 4000000:
-            # need to size it down if it's over 4MB
+        if os.fstat(uploaded_file.fileno()).st_size > 1000000:
+            # need to size it down if it's over 1MB
             print ("reduce image size")
 
             img = Image.open(uploaded_file.stream)
             x, y = img.size
-            x2, y2 = math.floor(x / 4), math.floor(y / 4)
+            print ("image size: " + str(x) + ":" + str(y))
+            x2, y2 = math.floor(x / 8), math.floor(y / 8)
             img = img.resize((x2, y2), Image.ANTIALIAS)
             byte_io = io.BytesIO()
             #img.save(byte_io, quality=50)
-            img.save(byte_io, format='PNG', quality=50)
+            img.save(byte_io, format='PNG', quality=25)
+            x, y = img.size
+            print ("image size: " + str(x) + ":" + str(y))
 
             print ("storing image size: " + str(byte_io.getbuffer().nbytes/1024/1024) + " MB")
             photo.data = Binary(byte_io.getvalue())
