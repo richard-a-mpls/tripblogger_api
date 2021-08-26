17de5e5e5168097400bdf2c9fbfa4eb634f7e53a
mousedrool <richard.abrahamson@yahoo.com>
2021-08-26 15:04:03 -0500

    for now always search all user projects

--- source/main/generated/swagger_server/controllers/published_project_controller.py
+++ source/main/generated/swagger_server/controllers/published_project_controller.py
@@ -35,7 +35,7 @@ def get_public_projects():  # noqa: E501
     if connexion.request.authorization is not None and "profile_id" in connexion.request.authorization:
         profile_id = connexion.request.authorization["profile_id"]
     m_interface = MongoInterface()
-    res_count, results = m_interface.get_published_projects("public", profile_id)
+    res_count, results = m_interface.get_published_projects("public", None)
     resp_list = list()
     for res in results:
         resp_list.append(Project.from_dict(res))
