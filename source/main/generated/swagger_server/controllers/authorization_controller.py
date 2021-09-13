5646f32cf3e77ff6a01acbc0bf66fa9342c91c2e
mousedrool <richard.abrahamson@yahoo.com>
2021-09-13 13:45:17 -0500

    consolidate to single b2c token.

--- source/main/generated/swagger_server/controllers/authorization_controller.py
+++ source/main/generated/swagger_server/controllers/authorization_controller.py
@@ -2,16 +2,14 @@ import connexion
 import time
 import jwt
 import os
+from swagger_server.utilities import b2c_token_validator
 
 """
 controller generated to handled auth operation described at:
 https://connexion.readthedocs.io/en/latest/security.html
 """
 def check_BearerAuth(token):
-    decoded_jwt = jwt.decode(token, os.environ["jwt_secret"], algorithms=["HS256"])
-    if int(time.time()) > decoded_jwt["expires"]:
-        print ("JWT has expired")
-        return None
+    decoded_jwt = b2c_token_validator.extract_token(token)
     connexion.request.authorization = decoded_jwt
     return decoded_jwt
 
