335828579bdda1c2863d3b0f6cdd1fcbc76557aa
mousedrool <richard.abrahamson@yahoo.com>
2021-09-13 13:44:45 -0500

    updates after removing no longer needed files.

--- source/main/generated/swagger_server/models/__init__.py
+++ source/main/generated/swagger_server/models/__init__.py
@@ -3,8 +3,6 @@
 # flake8: noqa
 from __future__ import absolute_import
 # import models into model package
-from swagger_server.models.api_session import ApiSession
-from swagger_server.models.authorization_request import AuthorizationRequest
 from swagger_server.models.photo import Photo
 from swagger_server.models.photos_body import PhotosBody
 from swagger_server.models.profile import Profile
