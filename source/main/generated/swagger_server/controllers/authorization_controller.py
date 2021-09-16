330e5c3839ef04201a2210caf9b9c780e42970e4
mousedrool <richard.abrahamson@yahoo.com>
2021-09-16 09:40:15 -0500

    remove no longer needed validator

--- source/main/generated/swagger_server/controllers/authorization_controller.py
+++ source/main/generated/swagger_server/controllers/authorization_controller.py
@@ -12,17 +12,3 @@ def check_BearerAuth(token):
     decoded_jwt = b2c_token_validator.extract_token(token)
     connexion.request.authorization = decoded_jwt
     return decoded_jwt
-
-def check_OptionalBearerAuth(token):
-    # in this case, it's ok if the user isn't authenticated, but if they are
-    # we need to knwo who they are.
-    try:
-        decoded_jwt = jwt.decode(token, os.environ["jwt_secret"], algorithms=["HS256"])
-        if int(time.time()) > decoded_jwt["expires"]:
-            print ("JWT has expired")
-            return None
-        connexion.request.authorization = decoded_jwt
-        return decoded_jwt
-    except:
-        print ("user not authenticated which is ok")
-        return {}
